3c2ae2a33660017aef49bd038a6f56a03fb577be
Caio Fontes <olixis@gmail.com>
2019-01-11 12:45:02 -0300

    readme changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src/components/App";
 import { BrowserRouter } from "react-router-dom";
-import "../src/styles/index.css";
+import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
